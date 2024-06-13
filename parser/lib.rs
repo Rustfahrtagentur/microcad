3ca76dc0c001af071765cf8fb3da9a4db445526a
Michael Winkelmann <michael@winkelmann.site>
2024-06-13 18:26:14 +0200

    test_file

--- parser/lib.rs
+++ parser/lib.rs
@@ -7,4 +7,23 @@ use pest_derive::Parser;
 #[grammar = "grammar.pest"]
 struct CsglParser;
 
-include!(concat!(env!("OUT_DIR"), "/pest_test.rs"));
+//include!(concat!(env!("OUT_DIR"), "/pest_test.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_file_nested() {
+        let test_filename = "tests/nested.csg";
+        let input = std::fs::read_to_string(test_filename)
+            .expect(format!("Test file not found: {}", test_filename).as_str());
+
+        match CsglParser::parse(Rule::r#code, &input) {
+            Ok(_) => (),
+            Err(e) => {
+                panic!("Failed parsing file in {}: {}", test_filename, e);
+            }
+        }
+    }
+}
