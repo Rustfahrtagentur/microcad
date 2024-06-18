11edd9ee95d9c31192685e5749788181a55aeb76
Michael Winkelmann <michael@winkelmann.site>
2024-06-18 22:34:27 +0200

    WIP from_syntax_node recurse

--- scenegraph/lib.rs
+++ scenegraph/lib.rs
@@ -15,21 +15,11 @@ impl TreeBuilder {
     pub fn from_syntax_node(syntax_node: SyntaxNode) -> Node {
         let root = Node::new(NodeInner::Root);
 
-        for child in syntax_node.children() {
-            let c = child.borrow();
-
-            match c.kind() {
-                SyntaxNodeKind::ObjectNode(object_node) => {}
-                _ => {}
-            }
-        }
-
+        Self::_from_syntax_node(root.clone(), syntax_node);
         root
     }
 
     fn _from_syntax_node(parent: Node, syntax_node: SyntaxNode) {
-        let node = Node::new(NodeInner::Root);
-
         for child in syntax_node.children() {
             let c = child.borrow();
 
