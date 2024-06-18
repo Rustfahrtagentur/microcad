60c31f543d708af6d5e8211ca67a9d4f24db1561
Michael Winkelmann <michael@winkelmann.site>
2024-06-18 22:28:34 +0200

    WIP From Syntax Node

--- scenegraph/lib.rs
+++ scenegraph/lib.rs
@@ -1,1 +1,42 @@
 mod polygon2d;
+
+use csg_parser::syntaxtree::{SyntaxNode, SyntaxNodeKind};
+
+pub enum NodeInner {
+    Root,
+    Shape2D(Box<dyn polygon2d::Primitive>),
+}
+
+pub type Node = rctree::Node<NodeInner>;
+
+struct TreeBuilder;
+
+impl TreeBuilder {
+    pub fn from_syntax_node(syntax_node: SyntaxNode) -> Node {
+        let root = Node::new(NodeInner::Root);
+
+        for child in syntax_node.children() {
+            let c = child.borrow();
+
+            match c.kind() {
+                SyntaxNodeKind::ObjectNode(object_node) => {}
+                _ => {}
+            }
+        }
+
+        root
+    }
+
+    fn _from_syntax_node(parent: Node, syntax_node: SyntaxNode) {
+        let node = Node::new(NodeInner::Root);
+
+        for child in syntax_node.children() {
+            let c = child.borrow();
+
+            match c.kind() {
+                SyntaxNodeKind::ObjectNode(object_node) => {}
+                _ => {}
+            }
+        }
+    }
+}
