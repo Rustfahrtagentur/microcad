443fa20ba054f4ac9edfb097e2c7de7138e21609
Michael Winkelmann <michael@winkelmann.site>
2024-06-19 23:12:00 +0200

    match qualified name

--- scenegraph/lib.rs
+++ scenegraph/lib.rs
@@ -24,7 +24,28 @@ impl TreeBuilder {
             let c = child.borrow();
 
             match c.kind() {
-                SyntaxNodeKind::ObjectNode(object_node) => {}
+                SyntaxNodeKind::ObjectNode(object_node) => {
+                    let mut node = None;
+                    match object_node.qualified_name().to_string().as_str() {
+                        "circle" => {
+                            // Todo: Parse arguments
+                            let circle = polygon2d::Circle {
+                                radius: 5.0,
+                                points: 10,
+                            };
+                            node = Some(Node::new(NodeInner::Shape2D(Box::new(circle))));
+                        }
+                        "rectangle" => {
+                            // Todo: Create rectangle
+                        }
+                        _ => {}
+                    }
+                    if let Some(node) = node {
+                        parent.append(node.clone());
+                        Self::_from_syntax_node(node, child.clone());
+                    }
+                }
+                SyntaxNodeKind::Document(_) => {} // Ignore
                 _ => {}
             }
         }
