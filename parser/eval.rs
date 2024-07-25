c73c964f4231d09fa5e477efa200978c1d498202
Michael Winkelmann <michael@winkelmann.site>
2024-07-25 17:30:37 +0200

    eval.rs

---
+++ parser/eval.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+use crate::expression::Expression;
+use crate::syntax_tree::SyntaxNode;
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidOperation,
+    InvalidFormatString,
+    EvaluateToStringError,
+}
+
+// Context for evaluation
+pub struct Context {
+    node: Option<SyntaxNode>,
+    symbols: HashMap<String, SyntaxNode>,
+    //    type_registry: HashMap<String, SyntaxNode>,
+}
+
+impl Context {
+    pub fn new() -> Self {
+        Self {
+            node: None,
+            symbols: HashMap::new(),
+        }
+    }
+}
+
+pub trait Eval: Sized {
+    fn eval(self, context: &Context) -> Result<Box<Expression>, Error>;
+
+    fn eval_to_string(self, context: &Context) -> Result<String, Error> {
+        let result = self.eval(context)?;
+        match result.as_ref() {
+            Expression::NumberLiteral(n) => Ok(n.to_string()),
+            Expression::BoolLiteral(b) => Ok(b.to_string()),
+            Expression::StringLiteral(s) => Ok(s.clone()),
+            //Expression::ListExpression(list) => list.eval_to_string(context)?,
+            _ => Err(Error::EvaluateToStringError),
+        }
+    }
+}
