a14dfef75c894d8cda2cb7346be04e6b441189e3
Michael Winkelmann <michael@winkelmann.site>
2024-08-11 21:34:50 +0200

    math: remove first argument

--- std/math.rs
+++ std/math.rs
@@ -14,16 +14,8 @@ impl ModuleBuilder {
         }
     }
 
-    pub fn builtin_function(
-        &mut self,
-        name: &str,
-        f: &'static BuiltinFunctionFunctor,
-    ) -> &mut Self {
-        self.module
-            .add_symbol(Symbol::BuiltinFunction(BuiltinFunction {
-                name: name.into(),
-                f,
-            }));
+    pub fn builtin_function(&mut self, f: BuiltinFunction) -> &mut Self {
+        self.module.add_symbol(Symbol::BuiltinFunction(f));
         self
     }
 
@@ -33,7 +25,7 @@ impl ModuleBuilder {
 }
 
 macro_rules! arg_1 {
-    ($f:ident($name:ident) for $($ty:tt),+) => { &|args, _| {
+    ($f:ident($name:ident) for $($ty:tt),+) => { BuiltinFunction::new(stringify!($f).into(), &|args, _| {
         match args.arg_1(stringify!(name))? {
             $(Value::$ty($name) => Ok(Value::$ty($name.$f())),)*
             Value::List(v) => {
@@ -48,177 +40,147 @@ macro_rules! arg_1 {
             }
             v => Err(Error::InvalidArgumentType(v.ty())),
         }
-    }
+    })
     };
     ($f:ident($name:ident) $inner:expr) => {
-        &|args, _| {
+        BuiltinFunction::new(stringify!($f).into(), &|args, _| {
             let l = |$name| $inner;
             l(args.arg_1(stringify!($name))?.clone())
-    }
+    })
 }
 }
 
 macro_rules! arg_2 {
     ($f:ident($x:ident, $y:ident) $inner:expr) => {
-        &|args, _| {
+        BuiltinFunction::new(stringify!($f).into(), &|args, _| {
             let l = |$x, $y| $inner;
             let (x, y) = args.arg_2(stringify!($x), stringify!($y))?;
             l(x.clone(), y.clone())
-        }
+        })
     };
 }
 
 pub fn builtin_module() -> std::rc::Rc<ModuleDefinition> {
     ModuleBuilder::namespace("math")
         // abs(x): Absolute value of x
-        .builtin_function("abs", arg_1!(abs(x) for Scalar, Length, Angle, Integer))
+        .builtin_function(arg_1!(abs(x) for Scalar, Length, Angle, Integer))
         // sign(x): Sign of x
-        .builtin_function(
-            "sign",
-            arg_1!(sign(x) {
-                match x {
-                    Value::Scalar(x) => Ok(Value::Scalar(x.signum())),
-                    Value::Length(x) => Ok(Value::Scalar(x.signum())),
-                    Value::Angle(x) => Ok(Value::Scalar(x.signum())),
-                    Value::Integer(x) => Ok(Value::Integer(x.signum())),
-                    _ => Err(Error::InvalidArgumentType(x.ty())),
-                }
-            }),
-        )
+        .builtin_function(arg_1!(sign(x) {
+            match x {
+                Value::Scalar(x) => Ok(Value::Scalar(x.signum())),
+                Value::Length(x) => Ok(Value::Scalar(x.signum())),
+                Value::Angle(x) => Ok(Value::Scalar(x.signum())),
+                Value::Integer(x) => Ok(Value::Integer(x.signum())),
+                _ => Err(Error::InvalidArgumentType(x.ty())),
+            }
+        }))
         // floor(x): Floor of x
-        .builtin_function("floor", arg_1!(floor(x) for Scalar, Length, Angle))
+        .builtin_function(arg_1!(floor(x) for Scalar, Length, Angle))
         // ceil(x): Ceiling of x
-        .builtin_function("ceil", arg_1!(ceil(x) for Scalar, Length, Angle))
+        .builtin_function(arg_1!(ceil(x) for Scalar, Length, Angle))
         // round(x): Round of x
-        .builtin_function("round", arg_1!(round(x) for Scalar, Length, Angle))
+        .builtin_function(arg_1!(round(x) for Scalar, Length, Angle))
         // to_int(x): Convert x to integer
-        .builtin_function(
-            "to_int",
-            arg_1!(to_int(x) {
-                match x {
-                    Value::Scalar(x) => Ok(Value::Integer(x as i64)),
-                    Value::Length(x) => Ok(Value::Integer(x as i64)),
-                    Value::Angle(x) => Ok(Value::Integer(x as i64)),
-                    Value::Integer(x) => Ok(Value::Integer(x)),
-                    _ => Err(Error::InvalidArgumentType(x.ty())),
-                }
-            }),
-        )
+        .builtin_function(arg_1!(to_int(x) {
+            match x {
+                Value::Scalar(x) => Ok(Value::Integer(x as i64)),
+                Value::Length(x) => Ok(Value::Integer(x as i64)),
+                Value::Angle(x) => Ok(Value::Integer(x as i64)),
+                Value::Integer(x) => Ok(Value::Integer(x)),
+                _ => Err(Error::InvalidArgumentType(x.ty())),
+            }
+        }))
         // to_scalar(x): Convert x to scalar
-        .builtin_function(
-            "to_scalar",
-            arg_1!(to_scalar(x) {
-                match x {
-                    Value::Scalar(x) => Ok(Value::Scalar(x)),
-                    Value::Length(x) => Ok(Value::Scalar(x)),
-                    Value::Angle(x) => Ok(Value::Scalar(x)),
-                    Value::Integer(x) => Ok(Value::Scalar(x as Scalar)),
-                    _ => Err(Error::InvalidArgumentType(x.ty())),
-                }
-            }),
-        )
+        .builtin_function(arg_1!(to_scalar(x) {
+            match x {
+                Value::Scalar(x) => Ok(Value::Scalar(x)),
+                Value::Length(x) => Ok(Value::Scalar(x)),
+                Value::Angle(x) => Ok(Value::Scalar(x)),
+                Value::Integer(x) => Ok(Value::Scalar(x as Scalar)),
+                _ => Err(Error::InvalidArgumentType(x.ty())),
+            }
+        }))
         // min(x,y): Minimum of x and y
-        .builtin_function(
-            "min",
-            arg_2!(min(x, y) {
-                match (x, y) {
-                    (Value::Scalar(x), Value::Scalar(y)) => Ok(Value::Scalar(x.min(y))),
-                    (Value::Length(x), Value::Length(y)) => Ok(Value::Length(x.min(y))),
-                    (Value::Angle(x), Value::Angle(y)) => Ok(Value::Angle(x.min(y))),
-                    (Value::Integer(x), Value::Integer(y)) => Ok(Value::Integer(x.min(y))),
-                    (x,_) => Err(Error::InvalidArgumentType(x.ty())),
-                }
-            }),
-        )
+        .builtin_function(arg_2!(min(x, y) {
+            match (x, y) {
+                (Value::Scalar(x), Value::Scalar(y)) => Ok(Value::Scalar(x.min(y))),
+                (Value::Length(x), Value::Length(y)) => Ok(Value::Length(x.min(y))),
+                (Value::Angle(x), Value::Angle(y)) => Ok(Value::Angle(x.min(y))),
+                (Value::Integer(x), Value::Integer(y)) => Ok(Value::Integer(x.min(y))),
+                (x,_) => Err(Error::InvalidArgumentType(x.ty())),
+            }
+        }))
         // max(x,y): Maximum of x and y
-        .builtin_function(
-            "max",
-            arg_2!(max(x, y) {
-                match (x, y) {
-                    (Value::Scalar(x), Value::Scalar(y)) => Ok(Value::Scalar(x.max(y))),
-                    (Value::Length(x), Value::Length(y)) => Ok(Value::Length(x.max(y))),
-                    (Value::Angle(x), Value::Angle(y)) => Ok(Value::Angle(x.max(y))),
-                    (Value::Integer(x), Value::Integer(y)) => Ok(Value::Integer(x.max(y))),
-                    (x,_) => Err(Error::InvalidArgumentType(x.ty())),
-                }
-            }),
-        )
+        .builtin_function(arg_2!(max(x, y) {
+            match (x, y) {
+                (Value::Scalar(x), Value::Scalar(y)) => Ok(Value::Scalar(x.max(y))),
+                (Value::Length(x), Value::Length(y)) => Ok(Value::Length(x.max(y))),
+                (Value::Angle(x), Value::Angle(y)) => Ok(Value::Angle(x.max(y))),
+                (Value::Integer(x), Value::Integer(y)) => Ok(Value::Integer(x.max(y))),
+                (x,_) => Err(Error::InvalidArgumentType(x.ty())),
+            }
+        }))
         // sin(x): Sine of x
-        .builtin_function("sin", arg_1!(sin(x) for Scalar, Angle))
+        .builtin_function(arg_1!(sin(x) for Scalar, Angle))
         // cos(x): Cosine of x
-        .builtin_function("cos", arg_1!(cos(x) for Scalar, Angle))
+        .builtin_function(arg_1!(cos(x) for Scalar, Angle))
         // tan(x): Tangent of x
-        .builtin_function("tan", arg_1!(tan(x) for Scalar, Angle))
+        .builtin_function(arg_1!(tan(x) for Scalar, Angle))
         // asin(x): Arcsine of x
-        .builtin_function(
-            "asin",
-            arg_1!(asin(x) {
-                match x {
-                    Value::Scalar(x) => Ok(Value::Angle(x.asin())),
-                    _ => Err(Error::InvalidArgumentType(x.ty())),
-                }
-            }),
-        )
+        .builtin_function(arg_1!(asin(x) {
+            match x {
+                Value::Scalar(x) => Ok(Value::Angle(x.asin())),
+                _ => Err(Error::InvalidArgumentType(x.ty())),
+            }
+        }))
         // acos(x): Arccosine of x
-        .builtin_function(
-            "acos",
-            arg_1!(acos(x) {
-                match x {
-                    Value::Scalar(x) => Ok(Value::Angle(x.acos())),
-                    _ => Err(Error::InvalidArgumentType(x.ty())),
-                }
-            }),
-        )
+        .builtin_function(arg_1!(acos(x) {
+            match x {
+                Value::Scalar(x) => Ok(Value::Angle(x.acos())),
+                _ => Err(Error::InvalidArgumentType(x.ty())),
+            }
+        }))
         // atan(x): Arctangent of x
-        .builtin_function(
-            "atan",
-            arg_1!(atan(x) {
-                match x {
-                    Value::Scalar(x) => Ok(Value::Angle(x.atan())),
-                    _ => Err(Error::InvalidArgumentType(x.ty())),
-                }
-            }),
-        )
+        .builtin_function(arg_1!(atan(x) {
+            match x {
+                Value::Scalar(x) => Ok(Value::Angle(x.atan())),
+                _ => Err(Error::InvalidArgumentType(x.ty())),
+            }
+        }))
         // sqrt(x): Square root of x
-        .builtin_function("sqrt", arg_1!(sqrt(x) for Scalar))
+        .builtin_function(arg_1!(sqrt(x) for Scalar))
         // ln(x): Natural logarithm of x
-        .builtin_function("ln", arg_1!(ln(x) for Scalar))
+        .builtin_function(arg_1!(ln(x) for Scalar))
         // log2(x): Base 2 logarithm of x
-        .builtin_function("log2", arg_1!(log2(x) for Scalar))
+        .builtin_function(arg_1!(log2(x) for Scalar))
         // log10(x): Base 10 logarithm of x
-        .builtin_function("log10", arg_1!(log10(x) for Scalar))
+        .builtin_function(arg_1!(log10(x) for Scalar))
         // exp(x): Exponential of x
-        .builtin_function("exp", arg_1!(exp(x) for Scalar))
+        .builtin_function(arg_1!(exp(x) for Scalar))
         // pow(x,y): x raised to the power of y
-        .builtin_function(
-            "pow",
-            arg_2!(pow(x, y) {
-                match (x, y) {
-                    (Value::Scalar(x), Value::Scalar(y)) => Ok(Value::Scalar(x.powf(y))),
-                    (Value::Length(x), Value::Scalar(y)) => Ok(Value::Length(x.powf(y))),
-                    (Value::Angle(x), Value::Scalar(y)) => Ok(Value::Angle(x.powf(y))),
-                    (Value::Integer(x), Value::Integer(y)) => Ok(Value::Integer(x.pow(y as u32))),
-                    (Value::Scalar(x), Value::Integer(y)) => Ok(Value::Scalar(x.powf(y as Scalar))),
-                    (Value::Length(x), Value::Integer(y)) => Ok(Value::Length(x.powf(y as Scalar))),
-                    (Value::Angle(x), Value::Integer(y)) => Ok(Value::Angle(x.powf(y as Scalar))),
-                    (x,_) => Err(Error::InvalidArgumentType(x.ty())),
-                }
-            }),
-        )
+        .builtin_function(arg_2!(pow(x, y) {
+            match (x, y) {
+                (Value::Scalar(x), Value::Scalar(y)) => Ok(Value::Scalar(x.powf(y))),
+                (Value::Length(x), Value::Scalar(y)) => Ok(Value::Length(x.powf(y))),
+                (Value::Angle(x), Value::Scalar(y)) => Ok(Value::Angle(x.powf(y))),
+                (Value::Integer(x), Value::Integer(y)) => Ok(Value::Integer(x.pow(y as u32))),
+                (Value::Scalar(x), Value::Integer(y)) => Ok(Value::Scalar(x.powf(y as Scalar))),
+                (Value::Length(x), Value::Integer(y)) => Ok(Value::Length(x.powf(y as Scalar))),
+                (Value::Angle(x), Value::Integer(y)) => Ok(Value::Angle(x.powf(y as Scalar))),
+                (x,_) => Err(Error::InvalidArgumentType(x.ty())),
+            }
+        }))
         // length(x): Length of x
-        .builtin_function(
-            "length",
-            arg_1!(length(x) {
-                match x {
-                    Value::Vec2(x) => Ok(Value::Length(x.magnitude())),
-                    Value::Vec3(x) => Ok(Value::Length(x.magnitude())),
-                    Value::Vec4(x) => Ok(Value::Length(x.magnitude())),
-                    _ => Err(Error::InvalidArgumentType(x.ty())),
-                }
-            }),
-        )
+        .builtin_function(arg_1!(length(x) {
+            match x {
+                Value::Vec2(x) => Ok(Value::Length(x.magnitude())),
+                Value::Vec3(x) => Ok(Value::Length(x.magnitude())),
+                Value::Vec4(x) => Ok(Value::Length(x.magnitude())),
+                _ => Err(Error::InvalidArgumentType(x.ty())),
+            }
+        }))
         // normalize(x): Normalize x
-        .builtin_function("normalize", arg_1!(normalize(x) for Vec2, Vec3, Vec4))
+        .builtin_function(arg_1!(normalize(x) for Vec2, Vec3, Vec4))
         .build()
 }
 
@@ -242,4 +204,5 @@ fn test_builtin_function(name: &str, input: &str, expected: &str) {
 #[test]
 fn test_build_math_module() {
     test_builtin_function("abs", "math::abs(-1.0)", "1");
+    test_builtin_function("sqrt", "math::sqrt(4.0)", "2");
 }
