491a7d4c34e06636d01db7f31f28244ffaa1892c
Michael Winkelmann <michael@winkelmann.site>
2024-08-15 18:40:25 +0200

    std: ModuleBuilder

--- std/lib.rs
+++ std/lib.rs
@@ -1,12 +1,75 @@
 mod math;
 
-use microcad_parser::language::module::*;
+use microcad_parser::eval::*;
+use microcad_parser::language::{function::*, module::*};
+
+pub struct ModuleBuilder {
+    module: ModuleDefinition,
+}
+
+impl ModuleBuilder {
+    pub fn namespace(name: &str) -> ModuleBuilder {
+        Self {
+            module: ModuleDefinition::namespace(name.into()),
+        }
+    }
+
+    pub fn builtin_function(&mut self, f: BuiltinFunction) -> &mut Self {
+        self.module.add_symbol(Symbol::BuiltinFunction(f));
+        self
+    }
+
+    pub fn module(&mut self, m: std::rc::Rc<ModuleDefinition>) -> &mut Self {
+        self.module.add_module(m);
+        self
+    }
+
+    pub fn build(&mut self) -> std::rc::Rc<ModuleDefinition> {
+        std::rc::Rc::new(self.module.clone())
+    }
+}
+
+/// @todo: Check if is possible to rewrite this macro with arbitrary number of arguments
+#[macro_export]
+macro_rules! arg_1 {
+    ($f:ident($name:ident) for $($ty:tt),+) => { BuiltinFunction::new(stringify!($f).into(), &|args, _| {
+        match args.arg_1(stringify!(name))? {
+            $(Value::$ty($name) => Ok(Value::$ty($name.$f())),)*
+            Value::List(v) => {
+                let mut result = ValueList::new();
+                for x in v.iter() {
+                    match x {
+                        $(Value::$ty(x) => result.push(Value::$ty(x.$f())),)*
+                        _ => return Err(Error::InvalidArgumentType(x.ty())),
+                    }
+                }
+                Ok(Value::List(List(result, v.ty())))
+            }
+            v => Err(Error::InvalidArgumentType(v.ty())),
+        }
+    })
+    };
+    ($f:ident($name:ident) $inner:expr) => {
+        BuiltinFunction::new(stringify!($f).into(), &|args, _| {
+            let l = |$name| $inner;
+            l(args.arg_1(stringify!($name))?.clone())
+    })
+}
+}
+
+#[macro_export]
+macro_rules! arg_2 {
+    ($f:ident($x:ident, $y:ident) $inner:expr) => {
+        BuiltinFunction::new(stringify!($f).into(), &|args, _| {
+            let l = |$x, $y| $inner;
+            let (x, y) = args.arg_2(stringify!($x), stringify!($y))?;
+            l(x.clone(), y.clone())
+        })
+    };
+}
 
 pub fn builtin_module() -> std::rc::Rc<ModuleDefinition> {
-    let mut module = ModuleDefinition::namespace("std".into());
-    module.add_module(math::builtin_module());
-    //module.add_module(geo2d::builtin_module());
-    //module.add_module(algorithm::builtin_module());
-
-    std::rc::Rc::new(module)
+    ModuleBuilder::namespace("std")
+        .module(math::builtin_module())
+        .build()
 }
--- std/math.rs
+++ std/math.rs
@@ -1,64 +1,8 @@
+use crate::{arg_1, arg_2, ModuleBuilder};
 use cgmath::InnerSpace;
 use microcad_parser::eval::*;
 use microcad_parser::language::lang_type::Ty;
 use microcad_parser::language::{function::*, module::*, value::*};
-
-struct ModuleBuilder {
-    module: ModuleDefinition,
-}
-
-impl ModuleBuilder {
-    pub fn namespace(name: &str) -> ModuleBuilder {
-        Self {
-            module: ModuleDefinition::namespace(name.into()),
-        }
-    }
-
-    pub fn builtin_function(&mut self, f: BuiltinFunction) -> &mut Self {
-        self.module.add_symbol(Symbol::BuiltinFunction(f));
-        self
-    }
-
-    pub fn build(&mut self) -> std::rc::Rc<ModuleDefinition> {
-        std::rc::Rc::new(self.module.clone())
-    }
-}
-
-macro_rules! arg_1 {
-    ($f:ident($name:ident) for $($ty:tt),+) => { BuiltinFunction::new(stringify!($f).into(), &|args, _| {
-        match args.arg_1(stringify!(name))? {
-            $(Value::$ty($name) => Ok(Value::$ty($name.$f())),)*
-            Value::List(v) => {
-                let mut result = ValueList::new();
-                for x in v.iter() {
-                    match x {
-                        $(Value::$ty(x) => result.push(Value::$ty(x.$f())),)*
-                        _ => return Err(Error::InvalidArgumentType(x.ty())),
-                    }
-                }
-                Ok(Value::List(List(result, v.ty())))
-            }
-            v => Err(Error::InvalidArgumentType(v.ty())),
-        }
-    })
-    };
-    ($f:ident($name:ident) $inner:expr) => {
-        BuiltinFunction::new(stringify!($f).into(), &|args, _| {
-            let l = |$name| $inner;
-            l(args.arg_1(stringify!($name))?.clone())
-    })
-}
-}
-
-macro_rules! arg_2 {
-    ($f:ident($x:ident, $y:ident) $inner:expr) => {
-        BuiltinFunction::new(stringify!($f).into(), &|args, _| {
-            let l = |$x, $y| $inner;
-            let (x, y) = args.arg_2(stringify!($x), stringify!($y))?;
-            l(x.clone(), y.clone())
-        })
-    };
-}
 
 pub fn builtin_module() -> std::rc::Rc<ModuleDefinition> {
     ModuleBuilder::namespace("math")
