a11d26eb7135c9d1ab27554e3482bd15b8a012c6
Michael Winkelmann <michael@winkelmann.site>
2024-08-16 21:36:16 +0200

    Tree: Group nodes and export nodes

--- render/tree.rs
+++ render/tree.rs
@@ -15,6 +15,9 @@ pub enum NodeInner {
     // A root node that only contains children
     Root,
 
+    /// A group node that contains children
+    Group,
+
     /// A trait that generates a 2D geometry, e.g. a primitive like a circle
     Generator2D(Box<dyn geo2d::Generator>),
 
@@ -26,16 +29,21 @@ pub enum NodeInner {
 
     // An affine transformation of a geometry
     Transform(Transform),
+
+    // An export node that exports the geometry to a file
+    Export(String),
 }
 
 impl Debug for NodeInner {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             NodeInner::Root => write!(f, "Root"),
+            NodeInner::Group => write!(f, "Group"),
             NodeInner::Generator2D(_) => write!(f, "Generator2D"),
             NodeInner::Geometry2D(_) => write!(f, "Geometry2D"),
             NodeInner::Algorithm(_) => write!(f, "Algorithm"),
             NodeInner::Transform(_) => write!(f, "Transform"),
+            NodeInner::Export(_) => write!(f, "Export"),
         }
     }
 }
@@ -44,4 +52,8 @@ pub type Node = rctree::Node<NodeInner>;
 
 pub fn root() -> Node {
     Node::new(NodeInner::Root)
+}
+
+pub fn group() -> Node {
+    Node::new(NodeInner::Group)
 }
