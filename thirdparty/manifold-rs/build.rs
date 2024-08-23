8d385d573af5d3784c054bc47411a65f77899890
Michael Winkelmann <michael@winkelmann.site>
2024-08-23 02:38:41 +0200

    Clean up

--- thirdparty/manifold-rs/build.rs
+++ thirdparty/manifold-rs/build.rs
@@ -1,30 +1,21 @@
 use cmake::Config;
 
 fn main() {
-    // Environment variable CMAKE_PREFIX_PATH is used to find the manifold library
-    // On Windows, set up rustup as follows: to use the GNU toolchain
-    // `$ rustup default stable-x86_64-pc-windows-gnu`
-
-    // Uncomment the following line to set the CMAKE_PREFIX_PATH environment variable
-    //std::env::set_var("CMAKE_PREFIX_PATH", "C:/msys64/mingw64/bin");
     let out_dir = std::env::var("OUT_DIR").unwrap();
 
     std::env::set_var("CMAKE_PREFIX_PATH", format!("{out_dir}/build/glm"));
-
     std::env::set_var("CMAKE_GENERATOR", "Ninja");
     std::env::set_var("CMAKE_BUILD_TYPE", "Release");
 
     let glm = Config::new("../glm").cxxflag("/EHsc").build();
     println!("cargo:rustc-link-search=native={}", glm.display());
 
-    let manifold = Config::new("../manifold")
+    Config::new("../manifold")
         .cxxflag("/EHsc")
         .define("CMAKE_BUILD_TYPE", "Release")
         .build();
 
     println!("cargo:rustc-link-search={out_dir}/lib");
-    //    println!("cargo:rustc-link-search={out_dir}/build/src/polygon");
-    println!("cargo:rustc-link-search=native={}", manifold.display());
 
     cxx_build::bridge("src/lib.rs")
         .std("c++17")
