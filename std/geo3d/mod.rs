b762b801452b1b37c930eefd65e872129e848144
Michael Winkelmann <michael@winkelmann.site>
2024-08-28 22:10:09 +0200

    geo3d builtin module

--- std/geo3d/mod.rs
+++ std/geo3d/mod.rs
@@ -1,6 +1,9 @@
+use microcad_builtin_proc_macro::DefineBuiltInRenderable3D;
 use microcad_core::*;
+use microcad_parser::{eval::*, language::*};
 use microcad_render::{RenderHash, Renderable3D};
 
+#[derive(DefineBuiltInRenderable3D)]
 pub struct Sphere {
     pub radius: Scalar,
 }
@@ -12,7 +15,10 @@ impl RenderHash for Sphere {
 }
 
 impl Renderable3D for Sphere {
-    fn render_geometry(&self, renderer: &mut dyn render::Renderer3D) -> Result<geo3d::Geometry> {
+    fn render_geometry(
+        &self,
+        renderer: &mut dyn render::Renderer3D,
+    ) -> microcad_core::Result<geo3d::Geometry> {
         use std::f64::consts::PI;
         let n = (self.radius / renderer.precision() * PI * 0.5).max(3.0) as u32;
 
@@ -21,4 +27,39 @@ impl Renderable3D for Sphere {
             n,
         )))
     }
+}
+
+#[derive(DefineBuiltInRenderable3D)]
+pub struct Cube {
+    pub size_x: Scalar,
+    pub size_y: Scalar,
+    pub size_z: Scalar,
+}
+
+impl RenderHash for Cube {
+    fn render_hash(&self) -> Option<u64> {
+        None
+    }
+}
+
+impl Renderable3D for Cube {
+    fn render_geometry(
+        &self,
+        _renderer: &mut dyn render::Renderer3D,
+    ) -> microcad_core::Result<geo3d::Geometry> {
+        Ok(geo3d::Geometry::Manifold(geo3d::Manifold::cube(
+            self.size_x,
+            self.size_y,
+            self.size_z,
+        )))
+    }
+}
+
+use crate::ModuleBuilder;
+
+pub fn builtin_module() -> std::rc::Rc<ModuleDefinition> {
+    ModuleBuilder::namespace("geo3d")
+        .add_builtin_module(Sphere::builtin_module())
+        .add_builtin_module(Cube::builtin_module())
+        .build()
 }
