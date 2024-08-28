f6860fdf33e9b29232893a49146e03a098eaf5c1
Michael Winkelmann <michael@winkelmann.site>
2024-08-28 23:26:25 +0200

    Simplify boolean_op algorithm

--- core/algorithm/boolean_op.rs
+++ core/algorithm/boolean_op.rs
@@ -5,42 +5,120 @@ pub enum BooleanOp {
     Intersection,
 }
 
-use crate::render::{Node, NodeInner, Renderable2D, Renderer2D};
-use crate::{geo2d::*, Algorithm};
-use geo::MultiPolygon;
+use crate::render::{Node, NodeInner, Renderer2D};
+use crate::Algorithm;
+use geo::OpType;
+
+fn into_group(node: Node) -> Option<Node> {
+    node.first_child().and_then(|n| {
+        if let NodeInner::Group = *n.borrow() {
+            Some(n.clone())
+        } else {
+            None
+        }
+    })
+}
+
+impl From<BooleanOp> for OpType {
+    fn from(op: BooleanOp) -> Self {
+        match op {
+            BooleanOp::Difference => OpType::Difference,
+            BooleanOp::Union => OpType::Union,
+            BooleanOp::Intersection => OpType::Intersection,
+            BooleanOp::Xor => OpType::Xor,
+        }
+    }
+}
+
+impl From<&BooleanOp> for OpType {
+    fn from(op: &BooleanOp) -> Self {
+        match op {
+            BooleanOp::Difference => OpType::Difference,
+            BooleanOp::Union => OpType::Union,
+            BooleanOp::Intersection => OpType::Intersection,
+            BooleanOp::Xor => OpType::Xor,
+        }
+    }
+}
 
 impl Algorithm for BooleanOp {
     fn process_2d(&self, renderer: &mut dyn Renderer2D, parent: Node) -> crate::Result<Node> {
-        let mut polygons = Vec::new();
+        let mut geos = Vec::new();
+
+        // all algorithm nodes are nested in a group
+        let group = into_group(parent).unwrap();
+
+        for child in group.children() {
+            let c = &*child.borrow();
+            let geo = match c {
+                NodeInner::Renderable2D(renderable) => renderable.request_geometry(renderer)?,
+                NodeInner::Geometry2D(g) => g.clone(),
+                NodeInner::Algorithm(algorithm) => {
+                    let new_node = algorithm.process_2d(renderer, child.clone())?;
+                    let c = &*new_node.borrow();
+                    match c {
+                        NodeInner::Geometry2D(g) => g.clone(),
+                        _ => continue,
+                    }
+                }
+                _ => continue,
+            };
+
+            geos.push(geo);
+        }
+
+        let mut result = geos[0].clone();
+        for (i, geo) in geos.iter().enumerate() {
+            if i == 0 {
+                continue;
+            }
+            if let Some(r) = result.boolean_op(geo.as_ref(), self) {
+                result = std::rc::Rc::new(r)
+            }
+        }
+
+        Ok(Node::new(NodeInner::Geometry2D(result)))
+    }
+    /*
+    fn process_3d(
+        &self,
+        renderer: &mut dyn crate::render::Renderer3D,
+        parent: Node,
+    ) -> crate::Result<Node> {
+        use crate::geo3d;
+        let mut manifolds = Vec::new();
 
         let mut new_nodes = Vec::new();
 
-        let handle_geo2d = |g: &Geometry, polygons: &mut Vec<MultiPolygon>| match g {
-            Geometry::MultiPolygon(p) => polygons.push(p.clone()),
+        let handle_geo3d = |g: &geo3d::Geometry, meshes: &mut Vec<geo3d::TriangleMesh>| match g {
+            geo3d::Geometry::Mesh(m) => manifolds.push(m.clone()),
+            geo3d::Geometry::Manifold(m) => meshes.push(m.mesh().into()),
             _ => unimplemented!("This should throw a warning"),
         };
 
-        let handle_renderable2d =
-            |renderer: &mut dyn Renderer2D,
-             renderable: &dyn Renderable2D,
-             polygons: &mut Vec<MultiPolygon>| match &*renderable
+        let handle_renderable3d =
+            |renderer: &mut dyn crate::render::Renderer3D,
+             renderable: &dyn crate::render::Renderable3D,
+             manifolds: &mut Vec<geo3d::Manifold>| match &*renderable
                 .request_geometry(renderer)
                 .unwrap()
             {
-                Geometry::MultiPolygon(p) => polygons.push(p.clone()),
+                geo3d::Geometry::Mesh(m) => manifolds.push(m.manifold().into()),
+                geo3d::Geometry::Manifold(m) => manifolds.push(m),
                 _ => unimplemented!("This should throw a warning"),
             };
+
         // TODO: This is a bit of a mess, we should probably refactor this
         // first_child() must be a Group node
         for child in parent.first_child().unwrap().children() {
             let c = &*child.borrow();
             match c {
-                NodeInner::Renderable2D(renderable) => {
-                    handle_renderable2d(renderer, &**renderable, &mut polygons)
+                NodeInner::Renderable3D(renderable) => {
+                    handle_renderable3d(renderer, &**renderable, &mut meshes)
                 }
-                NodeInner::Geometry2D(g) => handle_geo2d(g, &mut polygons),
+                NodeInner::Geometry3D(g) => handle_geo3d(g, &mut meshes),
                 NodeInner::Algorithm(algorithm) => {
-                    new_nodes.push(algorithm.process_2d(renderer, child.clone())?)
+                    new_nodes.push(algorithm.process_3d(renderer, child.clone())?)
                 }
                 _ => continue,
             }
@@ -49,17 +127,17 @@ impl Algorithm for BooleanOp {
         for node in new_nodes {
             let c = &*node.borrow();
             match c {
-                NodeInner::Geometry2D(g) => handle_geo2d(g, &mut polygons),
+                NodeInner::Geometry2D(g) => handle_geo3d(g, &mut meshes),
                 NodeInner::Renderable2D(generator) => {
-                    handle_renderable2d(renderer, generator.as_ref(), &mut polygons)
+                    handle_renderable3d(renderer, generator.as_ref(), &mut meshes)
                 }
                 _ => continue,
             }
         }
 
-        let mut result = polygons[0].clone();
+        let mut result = meshes[0].clone();
 
-        for (i, polygon) in polygons.iter().enumerate() {
+        for (i, polygon) in meshes.iter().enumerate() {
             if i == 0 {
                 continue;
             }
@@ -72,10 +150,10 @@ impl Algorithm for BooleanOp {
             }
         }
 
-        Ok(Node::new(NodeInner::Geometry2D(std::rc::Rc::new(
-            Geometry::MultiPolygon(result),
+        Ok(Node::new(NodeInner::Geometry3D(std::rc::Rc::new(
+            geo3d::Geometry::Mesh(geo3d::TriangleMesh::default()),
         ))))
-    }
+    }*/
 }
 
 pub fn difference() -> Node {
--- core/geo2d.rs
+++ core/geo2d.rs
@@ -1,10 +1,54 @@
 use crate::Scalar;
 pub type LineString = geo::LineString<Scalar>;
+pub type MultiLineString = geo::MultiLineString<Scalar>;
 pub type Polygon = geo::Polygon<Scalar>;
 pub type MultiPolygon = geo::MultiPolygon<Scalar>;
 pub type Rect = geo::Rect<Scalar>;
 pub type Point = geo::Point<Scalar>;
-pub type Geometry = geo::Geometry<Scalar>;
+
+pub enum Geometry {
+    LineString(LineString),
+    MultiLineString(MultiLineString),
+    Polygon(Polygon),
+    MultiPolygon(MultiPolygon),
+    Rect(Rect),
+    Point(Point),
+}
+
+impl Geometry {
+    pub fn try_convert_to_multi_polygon(&self) -> Option<MultiPolygon> {
+        match self {
+            Geometry::LineString(_) | Geometry::Point(_) | Geometry::MultiLineString(_) => None,
+            Geometry::Polygon(polygon) => Some(MultiPolygon::new(vec![polygon.clone()])),
+            Geometry::MultiPolygon(multi_polygon) => Some(multi_polygon.clone()),
+            Geometry::Rect(rect) => Some(MultiPolygon::new(vec![Self::rect_to_polygon(rect)])),
+        }
+    }
+
+    fn rect_to_polygon(rect: &Rect) -> Polygon {
+        use geo::line_string;
+        let line_string = line_string![
+            (x: rect.min().x, y: rect.min().y),
+            (x: rect.max().x, y: rect.min().y),
+            (x: rect.max().x, y: rect.max().y),
+            (x: rect.min().x, y: rect.max().y),
+            (x: rect.min().x, y: rect.min().y),
+        ];
+        Polygon::new(line_string, vec![])
+    }
+
+    pub fn boolean_op(
+        &self,
+        other: &Self,
+        op: &crate::algorithm::boolean_op::BooleanOp,
+    ) -> Option<Self> {
+        let a = self.try_convert_to_multi_polygon()?;
+        let b = other.try_convert_to_multi_polygon()?;
+        use geo::BooleanOps;
+        let result = a.boolean_op(&b, op.into());
+        Some(Geometry::MultiPolygon(result))
+    }
+}
 
 pub fn line_string_to_multi_polygon(line_string: LineString) -> MultiPolygon {
     MultiPolygon::new(vec![Polygon::new(line_string, vec![])])
