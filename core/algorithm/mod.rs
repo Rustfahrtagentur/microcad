2c7161785fccd727c459623feddcd07039cd3cad
Michael Winkelmann <michael@winkelmann.site>
2024-09-21 18:36:50 +0200

    Algorithm: Add doc

--- core/algorithm/mod.rs
+++ core/algorithm/mod.rs
@@ -14,10 +14,12 @@ pub use boolean_op::BooleanOp;
 
 /// Algorithm trait
 pub trait Algorithm {
+    /// Processes geometry for a 2d renderer and returns a geometry
     fn process_2d(&self, _renderer: &mut dyn Renderer2D, _parent: Node) -> Result<Node> {
         unimplemented!()
     }
 
+    /// Processes geometry for a 3d renderer and returns a geometry
     fn process_3d(&self, _renderer: &mut dyn Renderer3D, _parent: Node) -> Result<Node> {
         unimplemented!()
     }
