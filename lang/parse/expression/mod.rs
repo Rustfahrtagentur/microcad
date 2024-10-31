e72c999f815d1feeab8dd482f3ff185967bb094d
Michael Winkelmann <michael@winkelmann.site>
2024-10-31 19:41:10 +0100

    Expression: mod.rs clippy

--- lang/parse/expression/mod.rs
+++ lang/parse/expression/mod.rs
@@ -229,25 +229,21 @@ impl Eval for Expression {
                 let lhs = lhs.eval(context)?;
                 match lhs {
                     Value::NamedTuple(tuple) => {
-                        let value = tuple.get(&rhs).unwrap();
+                        let value = tuple.get(rhs).unwrap();
                         Ok(value.clone())
                     }
-                    Value::Node(node) => {
-                        match node.fetch(&rhs.to_string().into()) {
-                            Some(symbol) =>
-                                match symbol.as_ref() {
-                                    Symbol::Value(_, value) => Ok(value.clone()),
-                                    _ => unimplemented!(),
-                                }
-                            None => {
-                                use crate::diag::PushDiag;
-                                use anyhow::anyhow;
-                                context.error(self, anyhow!("Unknown field: {rhs}"))?;
-                                todo!("Return none value on unknown field error (field name = {rhs})")
-                            }
+                    Value::Node(node) => match node.fetch(&rhs.to_string().into()) {
+                        Some(symbol) => match symbol.as_ref() {
+                            Symbol::Value(_, value) => Ok(value.clone()),
                             _ => unimplemented!(),
+                        },
+                        None => {
+                            use crate::diag::PushDiag;
+                            use anyhow::anyhow;
+                            context.error(self, anyhow!("Unknown field: {rhs}"))?;
+                            todo!("Return none value on unknown field error (field name = {rhs})")
                         }
-                    }
+                    },
                     _ => unimplemented!(),
                 }
             }
