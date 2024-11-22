73a222f197ab0d3add6c8e7efec7fefe89f4c05e
Michael Winkelmann <michael@winkelmann.site>
2024-11-22 18:10:08 +0100

    CallArgument: get_named_match

--- lang/parse/call/call_argument.rs
+++ lang/parse/call/call_argument.rs
@@ -3,7 +3,11 @@
 
 //! A single call argument
 
-use crate::{errors::*, eval::*, ord_map::*, parse::*, parser::*, src_ref::*};
+use std::cell::Ref;
+
+use crate::{
+    errors::*, eval::*, ord_map::*, parse::*, parser::*, r#type::TypeAnnotation, src_ref::*,
+};
 
 /// Call argument
 #[derive(Clone, Debug)]
@@ -14,6 +18,31 @@ pub struct CallArgument {
     pub value: Expression,
     /// Source code reference
     src_ref: SrcRef,
+}
+
+impl CallArgument {
+    /// Evaluates the CallArgument and the parameter and return the matched value, if successful
+    pub fn get_named_match(&self, context: &mut Context, parameter: &Parameter) -> Result<Value> {
+        assert!(&self.name.is_some());
+
+        let arg_value = self.value.eval(context)?;
+        let param_value = parameter.eval(context)?;
+        if param_value.type_matches(&arg_value.ty()) {
+            Ok(arg_value)
+        } else {
+            use crate::diag::PushDiag;
+            context.error(
+                self,
+                anyhow::anyhow!(
+                    "Type mismatch for parameter `{name}: Expected {expected}, got {got}.",
+                    name = parameter.name,
+                    expected = arg_value.ty(),
+                    got = param_value.specified_type.unwrap()
+                ),
+            )?;
+            Ok(Value::Invalid)
+        }
+    }
 }
 
 impl SrcReferrer for CallArgument {
@@ -80,5 +109,49 @@ impl std::fmt::Display for CallArgument {
             Some(ref name) => write!(f, "{} = {}", name, self.value),
             None => write!(f, "{}", self.value),
         }
+    }
+}
+
+#[test]
+fn call_argument_match() {
+    let arg = |name: &str, i| -> CallArgument {
+        CallArgument {
+            name: if name.is_empty() {
+                None
+            } else {
+                Some(name.into())
+            },
+            value: Expression::Literal(Literal::Integer(Refer::none(i))),
+            src_ref: SrcRef(None),
+        }
+    };
+
+    let param = |name: &str, ty: Option<Type>, i: Option<i64>| -> Parameter {
+        Parameter::new(
+            name.into(),
+            ty.map(|ty| TypeAnnotation(Refer::none(ty))),
+            i.map(|i| Expression::Literal(Literal::Integer(Refer::none(i)))),
+            SrcRef(None),
+        )
+    };
+
+    use crate::r#type::Type;
+
+    let mut context = Context::default();
+    match arg("a", 10).get_named_match(&mut context, &param("a", Some(Type::Integer), Some(1))) {
+        Ok(Value::Integer(value)) => assert_eq!(value.value, 10, "Same value expected"),
+        Ok(value) => panic!("Value mismatch, expected integer: {value}"),
+        Err(err) => panic!("No match found: {err:?}"),
+    }
+
+    match arg("a", 10).get_named_match(&mut context, &param("a", Some(Type::Integer), None)) {
+        Ok(Value::Integer(value)) => assert_eq!(value.value, 10, "Same value expected"),
+        Ok(value) => panic!("Value mismatch, expected integer: {value}"),
+        Err(err) => panic!("No match found: {err:?}"),
+    }
+
+    match arg("a", 10).get_named_match(&mut context, &param("a", Some(Type::Angle), None)) {
+        Ok(value) => print!("Value mismatch, expected integer: {value}"),
+        Err(err) => panic!("No match found: {err:?}"),
     }
 }
