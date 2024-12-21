4f6e3677323de4c0c91fc03207409357a9203873
Patrick Hoffmann <p.hoffmann@berlin.de>
2024-12-22 00:15:17 +0100

    test: fix tes_simple_module_definition

--- tests/lib.rs
+++ tests/lib.rs
@@ -434,8 +434,8 @@ fn test_simple_module_definition() {
         _ => panic!("Expected module definition"),
     };
     assert_eq!(module_definition.body.pre_init_statements.len(), 1);
-    assert_eq!(module_definition.body.inits.len(), 0);
-    assert!(module_definition.body.init.is_some());
+    assert_eq!(module_definition.body.explicit_inits.len(), 0);
+    assert!(module_definition.body.implicit_init.is_some());
     assert_eq!(module_definition.body.post_init_statements.len(), 0);
 
     // Call the module definition of `donut` and verify it
@@ -488,8 +488,8 @@ fn test_module_definition_with_parameters() {
         _ => panic!("Expected module definition"),
     };
     assert_eq!(module_definition.body.pre_init_statements.len(), 1);
-    assert_eq!(module_definition.body.inits.len(), 0);
-    assert!(module_definition.body.init.is_some());
+    assert_eq!(module_definition.body.explicit_inits.len(), 0);
+    assert!(module_definition.body.implicit_init.is_some());
     assert_eq!(module_definition.body.post_init_statements.len(), 0);
 
     // Call the module definition of `donut` and verify it
@@ -564,7 +564,7 @@ fn module_definition_init() {
         _ => panic!("Expected module definition"),
     };
     assert_eq!(module_definition.body.pre_init_statements.len(), 1);
-    assert_eq!(module_definition.body.inits.len(), 2);
+    assert_eq!(module_definition.body.explicit_inits.len(), 2);
     assert_eq!(module_definition.body.post_init_statements.len(), 1);
 
     // Call the module definition of `donut` and verify it
