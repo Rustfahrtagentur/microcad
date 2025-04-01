042c23dce4bbe3ef4dbc66241039e4e8157b45fc
Patrick Hoffmann <p.hoffmann@berlin.de>
2025-04-01 18:15:10 +0200

    resolve: more comments

--- lang/resolve/resolve_context.rs
+++ lang/resolve/resolve_context.rs
@@ -9,6 +9,26 @@ pub struct ResolveContext {
 impl ResolveContext {
     /// Create new resolve context
     pub fn new(search_paths: Vec<std::path::PathBuf>) -> Self {
+        Self {
+            externals: Self::search_externals(search_paths),
+        }
+    }
+
+    /// search for an external file which may include a given qualified name
+    pub fn fetch_external(&self, name: QualifiedName) -> ResolveResult<&std::path::PathBuf> {
+        for (namespace, path) in self.externals.iter() {
+            if name.is_sub_of(namespace) {
+                eprintln!("found {name} in {namespace}");
+                return Ok(path);
+            }
+        }
+        Err(ResolveError::ExternalSymbolNotFound(name))
+    }
+
+    /// searches for external source code files (external modules) in some search paths
+    fn search_externals(
+        search_paths: Vec<std::path::PathBuf>,
+    ) -> std::collections::HashMap<QualifiedName, std::path::PathBuf> {
         let mut externals = std::collections::HashMap::new();
         search_paths.iter().for_each(|search_path| {
             Self::scan_path(search_path.clone(), crate::MICROCAD_EXTENSIONS)
@@ -25,20 +45,10 @@ impl ResolveContext {
                     );
                 });
         });
-        Self { externals }
+        externals
     }
 
-    /// search for an external file which may include a given qualified name
-    pub fn fetch_external(&self, name: QualifiedName) -> ResolveResult<&std::path::PathBuf> {
-        for (namespace, path) in self.externals.iter() {
-            if name.is_sub_of(namespace) {
-                eprintln!("found {name} in {namespace}");
-                return Ok(path);
-            }
-        }
-        Err(ResolveError::ExternalSymbolNotFound(name))
-    }
-
+    /// convert a path (of an external source code file) into a qualified name
     fn into_qualified_name(path: &std::path::Path) -> QualifiedName {
         use crate::src_ref::*;
 
@@ -54,6 +64,7 @@ impl ResolveContext {
         )
     }
 
+    /// scan in a specified path for all available files with one of the given extensions
     fn scan_path(
         search_path: std::path::PathBuf,
         extensions: &[&str],
