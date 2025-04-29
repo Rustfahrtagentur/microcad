a4b8da3d4e2a4cd1db065b5db0a07431a0c12499
Patrick Hoffmann <p.hoffmann@berlin.de>
2025-04-29 12:25:10 +0200

    eval: fix source_file not closing namespace

--- lang/eval/source_file.rs
+++ lang/eval/source_file.rs
@@ -4,6 +4,7 @@ impl Eval for SourceFile {
     fn eval(&self, context: &mut EvalContext) -> EvalResult<Value> {
         context.open_namespace(self.id());
         let result = Body::evaluate_vec(&self.body, context);
+        context.close_namespace();
         log::trace!("Evaluated context:\n{context}");
         result
     }
