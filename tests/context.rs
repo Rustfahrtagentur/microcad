d4ba6886be934daa759bc675270a25a37fd7b3cc
Patrick Hoffmann <p.hoffmann@berlin.de>
2025-05-02 20:52:30 +0200

    test: remove obsolete rust asserts in context test

--- tests/context.rs
+++ tests/context.rs
@@ -1,4 +1,4 @@
-use microcad_lang::{diag::Diag, eval::*, syntax::*};
+use microcad_lang::{eval::*, syntax::*};
 
 #[test]
 fn use_statements() {
@@ -12,23 +12,6 @@ fn use_statements() {
         Box::new(Stdout),
     );
     context.eval().expect("successful evaluation");
-
-    // test::foo::bar alias baz
-    assert!(context.lookup(&"baz".try_into().expect("name")).is_ok());
-    // std::geo2d::circle
-    assert!(context.lookup(&"circle".try_into().expect("name")).is_ok());
-    // std::print from std/module.µcad
-    assert!(context.lookup(&"print".try_into().expect("name")).is_ok());
-    // nodes from std::geo3d::*
-    assert!(context.lookup(&"cube".try_into().expect("name")).is_ok());
-    assert!(context.lookup(&"sphere".try_into().expect("name")).is_ok());
-
-    // global node from module
-    assert!(context
-        .lookup(&"use_test::my_module".try_into().expect("valid name"))
-        .is_ok());
-
-    println!("{}", context.errors_as_string());
 }
 
 #[test]
@@ -42,7 +25,5 @@ fn locals() {
         &["../lib".into()],
         Box::new(Stdout),
     );
-    if context.eval().is_err() {
-        println!("{}", context.errors_as_string());
-    }
+    context.eval().expect("successful evaluation");
 }
