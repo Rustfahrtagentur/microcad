03eb78f7e684a0fe5ebf9cbb5d04a4f75e5dfc8f
Michael Winkelmann <michael@winkelmann.site>
2025-05-04 14:43:18 +0200

    geo3d: Builtin

--- builtin/lib.rs
+++ builtin/lib.rs
@@ -5,9 +5,10 @@
 
 mod algorithm;
 mod assert;
+mod geo2d;
+mod geo3d;
 mod math;
 mod print;
-mod geo2d;
 
 mod namespace_builder;
 
@@ -16,9 +17,10 @@ pub use namespace_builder::NamespaceBuilder;
 
 pub(crate) use algorithm::*;
 pub(crate) use assert::*;
+pub(crate) use geo2d::geo2d;
+pub(crate) use geo3d::geo3d;
 pub(crate) use math::math;
 pub(crate) use print::print;
-pub(crate) use geo2d::geo2d;
 
 /// Build the standard module
 pub fn builtin_namespace() -> Symbol {
@@ -30,5 +32,6 @@ pub fn builtin_namespace() -> Symbol {
         .symbol(algorithm())
         .symbol(math())
         .symbol(geo2d())
+        .symbol(geo3d())
         .build()
 }
