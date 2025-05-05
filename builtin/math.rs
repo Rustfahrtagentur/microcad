6e3fcb44ab6bb9049667a014f8d3aa4f91bc022c
Michael Winkelmann <michael@winkelmann.site>
2025-05-05 22:39:27 +0200

    math: Fix abs

--- builtin/math.rs
+++ builtin/math.rs
@@ -15,7 +15,7 @@ fn abs() -> Symbol {
                 ctx.error(
                     arg,
                     EvalError::ParameterTypeMismatch {
-                        id: id.clone(),
+                        id: arg.name.clone().unwrap_or(Identifier::no_ref("x")),
                         expected: Type::Integer,
                         found: value.ty(),
                     },
