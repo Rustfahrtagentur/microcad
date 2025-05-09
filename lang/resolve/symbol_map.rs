f1af057a0a32af78f8e41dd813deeee1f137237a
Patrick Hoffmann <p.hoffmann@berlin.de>
2025-05-09 09:46:34 +0200

    resolve: fix missing rustdoc

--- lang/resolve/symbol_map.rs
+++ lang/resolve/symbol_map.rs
@@ -77,6 +77,7 @@ impl SymbolMap {
         self
     }
 
+    /// Print contained symbols with indention.
     pub fn print(&self, f: &mut std::fmt::Formatter<'_>, depth: usize) -> std::fmt::Result {
         for (id, symbol) in self.0.iter() {
             symbol.print_symbol(f, Some(id), depth)?;
