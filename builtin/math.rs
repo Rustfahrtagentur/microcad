b056a1e40dcd71ec4c8000a9d9845cce26f28237
Michael Winkelmann <michael@winkelmann.site>
2025-06-13 13:17:44 +0200

    Mat: __builtin rotate functions

--- builtin/math.rs
+++ builtin/math.rs
@@ -1,8 +1,8 @@
 // Copyright © 2024-2025 The µcad authors <info@ucad.xyz>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use microcad_core::Vec3;
-use microcad_lang::{diag::*, eval::*, resolve::*, syntax::*, ty::*, value::*};
+use microcad_core::{Mat3, Vec3};
+use microcad_lang::{diag::*, eval::*, parameter_value, resolve::*, syntax::*, ty::*, value::*};
 
 /// Absolute value abs(x)
 fn abs() -> Symbol {
@@ -11,6 +11,7 @@ fn abs() -> Symbol {
         let arg = args.get_single()?;
         Ok(match &arg.value {
             Value::Integer(i) => Value::Integer(i.abs()),
+            Value::Scalar(s) => Value::Scalar(s.abs()),
             value => {
                 ctx.error(
                     arg,
@@ -24,6 +25,119 @@ fn abs() -> Symbol {
             }
         })
     })
+}
+
+/// Helper function to get an angle from a field in an argument list.
+fn get_angle(args: &ArgumentMap, axis: &str) -> cgmath::Deg<f64> {
+    match args[&axis.try_into().expect("Valid identifier")] {
+        Value::Angle(angle) => cgmath::Deg::<f64>(angle),
+        _ => unreachable!(),
+    }
+}
+
+/// Helper function to return rotation X,Y,Z rotation matrices from an [`ArgumentMap`].
+fn rotation_matrices_xyz(args: &ArgumentMap) -> (Mat3, Mat3, Mat3) {
+    (
+        Mat3::from_angle_x(get_angle(args, "x")),
+        Mat3::from_angle_y(get_angle(args, "y")),
+        Mat3::from_angle_z(get_angle(args, "z")),
+    )
+}
+
+/// Rotate a vector around an axis.
+fn rotate_around_axis() -> Symbol {
+    Symbol::new_builtin(
+        Identifier::no_ref("rotate_around_axis"),
+        Some(
+            vec![
+                parameter_value!(angle: Angle),
+                parameter_value!(x: Scalar),
+                parameter_value!(y: Scalar),
+                parameter_value!(z: Scalar),
+            ]
+            .into(),
+        ),
+        &|_params, args, ctx| match ArgumentMap::find_match(
+            args,
+            _params.expect("ParameterValueList"),
+        ) {
+            Ok(ref args) => {
+                let angle = get_angle(args, "angle");
+                let axis = Vec3::new(
+                    args[&"x".try_into()?].try_scalar()?,
+                    args[&"y".try_into()?].try_scalar()?,
+                    args[&"z".try_into()?].try_scalar()?,
+                );
+
+                let matrix = Mat3::from_axis_angle(axis, angle);
+                Ok(Value::Matrix(Box::new(Matrix::Matrix3(matrix))))
+            }
+            Err(err) => {
+                ctx.error(args, err)?;
+                Ok(Value::None)
+            }
+        },
+    )
+}
+
+/// Rotate around X, Y, Z (in that order).
+fn rotate_xyz() -> Symbol {
+    Symbol::new_builtin(
+        Identifier::no_ref("rotate_xyz"),
+        Some(
+            vec![
+                parameter_value!(x: Angle),
+                parameter_value!(y: Angle),
+                parameter_value!(z: Angle),
+            ]
+            .into(),
+        ),
+        &|_params, args, ctx| match ArgumentMap::find_match(
+            args,
+            _params.expect("ParameterValueList"),
+        ) {
+            Ok(args) => {
+                let (x_matrix, y_matrix, z_matrix) = rotation_matrices_xyz(&args);
+                Ok(Value::Matrix(Box::new(Matrix::Matrix3(
+                    x_matrix * y_matrix * z_matrix,
+                ))))
+            }
+            Err(err) => {
+                ctx.error(args, err)?;
+                Ok(Value::None)
+            }
+        },
+    )
+}
+
+/// Rotate around Z, Y, X (in that order).
+fn rotate_zyx() -> Symbol {
+    Symbol::new_builtin(
+        Identifier::no_ref("rotate_zyx"),
+        Some(
+            vec![
+                parameter_value!(x: Angle),
+                parameter_value!(y: Angle),
+                parameter_value!(z: Angle),
+            ]
+            .into(),
+        ),
+        &|_params, args, ctx| match ArgumentMap::find_match(
+            args,
+            _params.expect("ParameterValueList"),
+        ) {
+            Ok(args) => {
+                let (x_matrix, y_matrix, z_matrix) = rotation_matrices_xyz(&args);
+                Ok(Value::Matrix(Box::new(Matrix::Matrix3(
+                    z_matrix * y_matrix * x_matrix,
+                ))))
+            }
+            Err(err) => {
+                ctx.error(args, err)?;
+                Ok(Value::None)
+            }
+        },
+    )
 }
 
 pub fn math() -> Symbol {
@@ -45,5 +159,8 @@ pub fn math() -> Symbol {
             Value::NamedTuple(Vec3::unit_z().into()),
         ))
         .symbol(abs())
+        .symbol(rotate_around_axis())
+        .symbol(rotate_xyz())
+        .symbol(rotate_zyx())
         .build()
 }
