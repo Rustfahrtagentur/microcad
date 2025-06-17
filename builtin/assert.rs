7081e896f2f8020ba69e53f6b9d1d156b8b99cfd
Michael Winkelmann <michael@winkelmann.site>
2025-06-17 18:34:44 +0200

    Clippy

--- builtin/assert.rs
+++ builtin/assert.rs
@@ -113,7 +113,7 @@ pub fn assert_invalid() -> Symbol {
 
 pub fn type_of() -> Symbol {
     let id = Identifier::from_str("type_of").expect("valid id");
-    Symbol::new_builtin(id, None, &|_, args, context| {
+    Symbol::new_builtin(id, None, &|_, args, _| {
         if let Ok(arg) = args.get_single() {
             let ty = arg.value.ty();
             return Ok(Value::String(ty.to_string()));
