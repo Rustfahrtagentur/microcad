a22273377b63191756ca2bb902dbd7760d0d327e
Michael Winkelmann <michael@winkelmann.site>
2025-07-12 14:30:41 +0200

    Watch command

--- tools/cli/commands/watch.rs
+++ tools/cli/commands/watch.rs
@@ -14,8 +14,32 @@ pub struct Watch {
 
 /// Run this command for a CLI.
 impl RunCommand for Watch {
-    fn run(&self, _: &crate::cli::Cli) -> anyhow::Result<()> {
-        println!("watch: {:?}", self.export_args.input);
-        Ok(())
+    fn run(&self, cli: &Cli) -> anyhow::Result<()> {
+        let mut watcher = Watcher::new()?;
+        let config = cli.fetch_config()?;
+        let export = &self.export_args;
+
+        if export.list {
+            let mut context = cli.make_context(&self.export_args.input)?;
+            let node = context.eval().expect("Valid node");
+            export.list_targets(&export.target_nodes(&node, &config, context.exporters())?)
+        } else {
+            // Recompile whenever something relevant happens.
+            loop {
+                let mut context = cli.make_context(&self.export_args.input)?;
+                // Re-evaluate context.
+                if let Ok(node) = context.eval() {
+                    let target_nodes = &export.target_nodes(&node, &config, context.exporters())?;
+
+                    export.export_targets(target_nodes)?;
+                }
+
+                // Watch all dependencies of the most recent compilation.
+                watcher.update(vec![self.export_args.input.clone()])?;
+
+                // Wait until anything relevant happens.
+                watcher.wait()?;
+            }
+        }
     }
 }
