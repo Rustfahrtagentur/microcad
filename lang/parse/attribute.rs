0963bdf76bfcd4b680825f5f2af29ab094a4e135
Michael Winkelmann <michael@winkelmann.site>
2025-07-21 15:15:49 +0200

    AttributeCommand: syntax and parse

--- lang/parse/attribute.rs
+++ lang/parse/attribute.rs
@@ -3,24 +3,69 @@
 
 use crate::{parse::*, parser::*};
 
+impl Parse for AttributeSubcommand {
+    fn parse(pair: Pair) -> ParseResult<Self> {
+        Parser::ensure_rule(&pair, Rule::attribute_subcommand);
+
+        for inner in pair.inner() {
+            match inner.as_rule() {
+                Rule::format_string => return Ok(Self::FormatString(FormatString::parse(inner)?)),
+                Rule::identifier => {
+                    return Ok(Self::Call(
+                        pair.find(Rule::identifier).expect("Identifier"),
+                        pair.find(Rule::argument_list),
+                    ));
+                }
+                _ => {}
+            }
+        }
+
+        unreachable!()
+    }
+}
+
+impl Parse for AttributeCommand {
+    fn parse(pair: Pair) -> ParseResult<Self> {
+        Parser::ensure_rule(&pair, Rule::attribute_command);
+
+        // TODO: Use try_collect() once this iterator is a stable feature.
+        let mut subcommands = Vec::new();
+        for pair in pair.inner() {
+            if pair.as_rule() == Rule::attribute_subcommand {
+                subcommands.push(AttributeSubcommand::parse(pair)?);
+            }
+        }
+
+        Ok(Self {
+            id: pair.find(Rule::identifier).expect("Identifier"),
+            subcommands,
+            src_ref: pair.src_ref(),
+        })
+    }
+}
+
 impl Parse for Attribute {
     fn parse(pair: Pair) -> ParseResult<Self> {
-        Parser::ensure_rules(
-            &pair,
-            &[Rule::attribute_name_value, Rule::attribute_named_tuple],
-        );
+        Parser::ensure_rule(&pair, Rule::attribute);
 
-        match pair.as_rule() {
-            Rule::attribute_name_value => Ok(Self::NameValue(
-                crate::find_rule!(pair, identifier)?,
-                crate::find_rule!(pair, expression)?,
-            )),
-            Rule::attribute_named_tuple => Ok(Self::Tuple(
-                crate::find_rule!(pair, identifier)?,
-                crate::find_rule!(pair, argument_list)?,
-            )),
+        let inner = pair.inner().next().expect("pair");
+
+        Ok(match inner.as_rule() {
+            Rule::attribute_name_value => Self::NameValue(
+                inner.find(Rule::identifier).expect("Identifier"),
+                inner.find(Rule::expression).expect("Expression"),
+            ),
+            Rule::attribute_exporter => Self::Exporter(
+                inner.find(Rule::identifier).expect("Identifier"),
+                inner.find(Rule::argument_list).unwrap_or_default(),
+            ),
+            Rule::attribute_command => Self::Command(
+                inner
+                    .find(Rule::attribute_command)
+                    .expect("Attribute command"),
+            ),
             _ => unreachable!(),
-        }
+        })
     }
 }
 
--- lang/syntax/attribute.rs
+++ lang/syntax/attribute.rs
@@ -6,21 +6,105 @@
 use crate::{src_ref::*, syntax::*};
 use derive_more::{Deref, DerefMut};
 
+/// A subcommand for an [`AttributeCommand`].
+#[derive(Debug, Clone)]
+pub enum AttributeSubcommand {
+    /// A format string subcommand: `"test.svg"`.
+    FormatString(FormatString),
+    /// A command with an identifier and optional arguments: `width(offset = 30mm)`.
+    Call(Identifier, Option<ArgumentList>),
+}
+
+impl std::fmt::Display for AttributeSubcommand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            AttributeSubcommand::FormatString(format_string) => write!(f, "{format_string}"),
+            AttributeSubcommand::Call(id, argument_list) => {
+                write!(
+                    f,
+                    "{id}{argument_list}",
+                    argument_list = match argument_list {
+                        Some(argument_list) => format!("({argument_list})"),
+                        None => String::new(),
+                    }
+                )
+            }
+        }
+    }
+}
+
+impl SrcReferrer for AttributeSubcommand {
+    fn src_ref(&self) -> SrcRef {
+        match &self {
+            AttributeSubcommand::FormatString(format_string) => format_string.src_ref(),
+            AttributeSubcommand::Call(identifier, argument_list) => match argument_list {
+                Some(argument_list) => {
+                    SrcRef::merge(&identifier.src_ref(), &argument_list.src_ref())
+                }
+                None => identifier.src_ref(),
+            },
+        }
+    }
+}
+
+/// An attribute command: `#[export: "test.svg"]`.
+#[derive(Debug, Clone)]
+pub struct AttributeCommand {
+    /// Command id.
+    pub id: Identifier,
+    /// Subcommands.
+    pub subcommands: Vec<AttributeSubcommand>,
+    /// Source code reference.
+    pub src_ref: SrcRef,
+}
+
+impl AttributeCommand {
+    /// Return command id.
+    pub fn id(&self) -> &Identifier {
+        &self.id
+    }
+}
+
+impl std::fmt::Display for AttributeCommand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{id}{subcommands}",
+            id = self.id,
+            subcommands = if self.subcommands.is_empty() {
+                String::new()
+            } else {
+                format!(
+                    ": {}",
+                    self.subcommands
+                        .iter()
+                        .map(|subcommand| subcommand.to_string())
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                )
+            }
+        )
+    }
+}
+
 /// An attribute item.
 #[derive(Debug, Clone)]
 pub enum Attribute {
-    /// A call attribute `foo::bar(baz = 32)`
-    Tuple(Identifier, ArgumentList),
-    /// A name value attribute `foo::baz = baz`
+    /// An exporter attribute: `#[bar(baz = 32)]`.
+    Exporter(Identifier, ArgumentList),
+    /// A name value attribute: `#[color = "red"]`.
     NameValue(Identifier, Expression),
+    /// A command attribute: `#[measure: width]`.
+    Command(AttributeCommand),
 }
 
 impl Attribute {
     /// Return qualified name for this attribute.
-    pub fn identifier(&self) -> &Identifier {
+    pub fn id(&self) -> &Identifier {
         match self {
-            Attribute::Tuple(id, _) => id,
+            Attribute::Exporter(id, _) => id,
             Attribute::NameValue(id, _) => id,
+            Attribute::Command(command) => command.id(),
         }
     }
 }
@@ -28,11 +112,14 @@ impl Attribute {
 impl std::fmt::Display for Attribute {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Attribute::Tuple(id, argument_list) => {
+            Attribute::Exporter(id, argument_list) => {
                 writeln!(f, "#[{id}({argument_list})]")
             }
             Attribute::NameValue(id, expression) => {
                 writeln!(f, "#[{id} = {expression}]")
+            }
+            Attribute::Command(command) => {
+                writeln!(f, "#[{command}]")
             }
         }
     }
@@ -41,8 +128,9 @@ impl std::fmt::Display for Attribute {
 impl SrcReferrer for Attribute {
     fn src_ref(&self) -> crate::src_ref::SrcRef {
         match self {
-            Attribute::Tuple(id, argument_list) => SrcRef::merge(id, argument_list),
+            Attribute::Exporter(id, argument_list) => SrcRef::merge(id, argument_list),
             Attribute::NameValue(id, expression) => SrcRef::merge(id, expression),
+            Attribute::Command(command) => command.src_ref.clone(),
         }
     }
 }
