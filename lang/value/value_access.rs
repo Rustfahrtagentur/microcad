b777e3c6882ae9eaba3e16633a1f27a0e7baa627
Michael Winkelmann <michael@winkelmann.site>
2025-09-02 21:38:29 +0200

    ValueAccess: by_str + get

--- lang/value/value_access.rs
+++ lang/value/value_access.rs
@@ -13,8 +13,8 @@ pub trait ValueAccess {
     /// Find unnamed value by type.
     fn by_ty(&self, ty: &Type) -> Option<&Value>;
 
-    /// Fetch an argument value by name as `&str`.
-    fn get<'a, T>(&'a self, id: &str) -> ValueResult<T>
+    /// Helper function to fetch an argument by string.
+    fn by_str<'a, T>(&'a self, id: &str) -> ValueResult<T>
     where
         T: std::convert::TryFrom<&'a Value>,
         T::Error: std::fmt::Debug,
@@ -32,6 +32,15 @@ pub trait ValueAccess {
         } else {
             Err(ValueError::IdNotFound(id.clone()))
         }
+    }
+
+    /// Fetch an argument value by name as `&str`.
+    fn get<'a, T>(&'a self, id: &str) -> T
+    where
+        T: std::convert::TryFrom<&'a Value>,
+        T::Error: std::fmt::Debug,
+    {
+        self.by_str(id).expect("No error")
     }
 
     /// Fetch an argument value by name as `&str`.
