7014cd4816512dd7780da326bfb875688580d20a
Michael Winkelmann <michael@winkelmann.site>
2025-09-02 21:34:03 +0200

    Value: mod argument_value and parameter_value

---
+++ lang/value/argument_value.rs
@@ -0,0 +1,60 @@
+// Copyright © 2024-2025 The µcad authors <info@ucad.xyz>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+//! Argument value evaluation entity
+
+use crate::{src_ref::*, syntax::*, ty::*, value::*};
+
+/// Argument value.
+#[derive(Clone, Debug)]
+pub struct ArgumentValue {
+    /// *value* of the argument.
+    pub value: Value,
+    /// If expression of value is a single identifier, this item catches it.
+    pub inline_id: Option<Identifier>,
+    /// Source code reference.
+    src_ref: SrcRef,
+}
+
+impl SrcReferrer for ArgumentValue {
+    fn src_ref(&self) -> SrcRef {
+        self.src_ref.clone()
+    }
+}
+
+impl std::fmt::Display for ArgumentValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{ty:?} = {val:?}",
+            val = self.value,
+            ty = self.value.ty()
+        )
+    }
+}
+
+impl Ty for ArgumentValue {
+    fn ty(&self) -> Type {
+        self.value.ty()
+    }
+}
+
+impl ArgumentValue {
+    /// Create new argument value
+    pub fn new(value: Value, inline_id: Option<Identifier>, src_ref: SrcRef) -> Self {
+        Self {
+            value,
+            inline_id,
+            src_ref,
+        }
+    }
+
+    /// If argument is an array returns the inner type
+    pub fn ty_inner(&self) -> Type {
+        if let Type::Array(ty) = self.ty() {
+            ty.as_ref().clone()
+        } else {
+            Type::Invalid
+        }
+    }
+}
---
+++ lang/value/parameter_value.rs
@@ -0,0 +1,75 @@
+// Copyright © 2024-2025 The µcad authors <info@ucad.xyz>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+//! Parameter value evaluation entity
+
+use crate::{src_ref::*, ty::*, value::*};
+
+/// Parameter value is the result of evaluating a parameter
+#[derive(Clone, Debug, Default)]
+pub struct ParameterValue {
+    /// Parameter type
+    pub specified_type: Option<Type>,
+    /// Parameter default
+    pub default_value: Option<Value>,
+    /// Source code reference
+    pub src_ref: SrcRef,
+}
+
+impl ParameterValue {
+    /// Creates an invalid parameter value, in case an error occurred during evaluation
+    pub fn invalid(src_ref: SrcRef) -> Self {
+        Self {
+            specified_type: None,
+            default_value: None,
+            src_ref,
+        }
+    }
+
+    /// Check if type of this parameter value matches the given one
+    pub fn type_matches(&self, ty: &Type) -> bool {
+        match &self.specified_type {
+            Some(t) => t == ty,
+            None => true, // Accept any type if none is specified
+        }
+    }
+}
+
+impl Ty for ParameterValue {
+    /// Return effective type
+    ///
+    /// Returns any `specified_type` or the type of the `default_value`.
+    /// Panics if neither of both is available.
+    fn ty(&self) -> Type {
+        if let Some(ty) = &self.specified_type {
+            ty.clone()
+        } else if let Some(def) = &self.default_value {
+            def.ty()
+        } else {
+            Type::Invalid
+        }
+    }
+}
+
+impl std::fmt::Display for ParameterValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(def) = &self.default_value {
+            write!(f, "{} = {def}", def.ty())?;
+        } else if let Some(ty) = &self.specified_type {
+            write!(f, "{ty}")?;
+        }
+        Ok(())
+    }
+}
+
+impl SrcReferrer for ParameterValue {
+    fn src_ref(&self) -> SrcRef {
+        self.src_ref.clone()
+    }
+}
+
+#[test]
+fn test_is_list_of() {
+    assert!(Type::Array(Box::new(QuantityType::Scalar.into()))
+        .is_array_of(&QuantityType::Scalar.into()));
+}
