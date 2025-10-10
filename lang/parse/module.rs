b100d391e6e5936309578a77a51637adc7c29b21
Patrick Hoffmann <p.hoffmann@berlin.de>
2025-10-10 19:06:18 +0200

    resolve: fix accidentally load external module when using mod with empty body

--- lang/parse/module.rs
+++ lang/parse/module.rs
@@ -5,12 +5,10 @@ use crate::{parse::*, parser::*, rc::*, syntax::*};
 
 impl Parse for Rc<ModuleDefinition> {
     fn parse(pair: Pair) -> ParseResult<Self> {
-        let body: Body = crate::find_rule!(pair, body)?;
-
         Ok(Rc::new(ModuleDefinition {
             visibility: crate::find_rule!(pair, visibility)?,
             id: crate::find_rule!(pair, identifier)?,
-            body: if body.is_empty() { None } else { Some(body) },
+            body: crate::find_rule_opt!(pair, body),
             src_ref: pair.clone().into(),
         }))
     }
