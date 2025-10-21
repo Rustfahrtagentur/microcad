bb0b84aaa04cc0fc4a13bfb86455be34fd9f91fe
Patrick Hoffmann <p.hoffmann@berlin.de>
2025-10-21 11:16:44 +0200

    scripts: publish.sh: provide hint about the expected tag name

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,13 +9,14 @@ command -v jq >/dev/null 2>&1 || { echo "Please install package 'jq'!"; exit 1; 
 
 ARGS=$@ # All arguments (e.g. `--dry-run`) will be appended to `cargo publish` 
 GIT_TAG=$(git describe --tags --exact-match 2>/dev/null)
+CRATE_VERSION=v`cargo metadata --no-deps --format-version 1 | jq -r '.packages[0].version'`
 
 if [ -z "$GIT_TAG" ]; then
     echo "Warning: You are NOT a on git tag, this means you can only publish in dry run mode!"
+    echo "Tag your branch with '$CRATE_VERSION' to publish in real."
     ARGS+=" --dry-run"
 else
     echo "You are on git tag: ${GIT_TAG}"
-    CRATE_VERSION=v`cargo metadata --no-deps --format-version 1 | jq -r '.packages[0].version'`
     
     if [ "$GIT_TAG" != "$CRATE_VERSION" ]; then
         echo "Error: Git tag ($GIT_TAG) does not match Cargo.toml version ($CRATE_VERSION)"
